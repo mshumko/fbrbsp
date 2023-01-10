026bced82c6737fa338df620c3338a38e583b43c
Mykhaylo Shumko <msshumko@gmail.com>
2023-01-10 14:46:44 -0500

    Got the Mag data time_range programmed.

--- fbrbsp/load/emfisis.py
+++ fbrbsp/load/emfisis.py
@@ -101,6 +101,45 @@ class Spec:
             self.data[key] = self.data[key][idt, :]
         return self.data
 
+    def spectrum(self, component='BuBu', fce=True, ax=None, pcolormesh_kwargs=None):
+        """
+        Plots a component of the EMFISIS WFR spectrum (controlled via the component 
+        kwarg) with optional f_ce lines superposed.
+
+        Parameters
+        ----------
+        component: str
+            The magnetic or electric field component. Can be one of
+            'BuBu', 'BvBv', 'BwBw', 'EuEu', 'EvEv', or 'EwEw'.
+        fce: bool
+            Plot the f_ce, f_ce/2, and f_ce/10 lines.
+        ax: plt.Axes
+            A subplot to plot on.
+        pcolormesh_kwarg: dict
+            The keyword arguments to pass into plt.pcolormesh. By default
+            the only setting is a logarithmic color scale.
+        """
+        if ax is None:
+            fig, self.ax = plt.subplots()
+        else:
+            self.ax = ax
+
+        if pcolormesh_kwargs is None:
+            pcolormesh_kwargs = {
+                'norm':matplotlib.colors.LogNorm()
+            }
+
+        p = self.ax.pcolormesh(self['epoch'], self.WFR_frequencies, 
+            self[component].T, shading='auto', **pcolormesh_kwargs)
+
+        if fce:
+            mag_data = Mag(self.sc_id, self.time_range)
+            mag_data.load()
+            _fce = mag_data.fce()
+            for multiple in [1, 0.5, 0.1]:
+                self.ax.plot(mag_data['Epoch'][::100], multiple*_fce[::100], c='k')
+        return p, self.ax
+
     def _find_file(self, file_date):
         _file_match = (f'rbsp-{self.sc_id.lower()}_{self.inst.lower()}-'
             f'spectral-matrix-diagonal-merged_emfisis-l2_{file_date:%Y%m%d}_v*.cdf')
@@ -142,51 +181,6 @@ class Spec:
             raise IndexError(f'Only slicing with integer keys is supported.')
 
 
-    def spectrum(self, component='BuBu', fce=False, ax=None, pcolormesh_kwargs=None):
-        """
-        Plots a component of the EMFISIS WFR spectrum (controlled via the component 
-        kwarg) with optional f_ce lines superposed.
-
-        Parameters
-        ----------
-        component: str
-            The magnetic or electric field component. Can be one of
-            'BuBu', 'BvBv', 'BwBw', 'EuEu', 'EvEv', or 'EwEw'.
-        fce: bool
-            Plot the f_ce, f_ce/2, and f_ce/10 lines.
-        ax: plt.Axes
-            A subplot to plot on.
-        pcolormesh_kwarg: dict
-            The keyword arguments to pass into plt.pcolormesh. By default
-            the only setting is a logarithmic color scale.
-        """
-        if ax is None:
-            fig, self.ax = plt.subplots()
-        else:
-            self.ax = ax
-
-        if pcolormesh_kwargs is None:
-            pcolormesh_kwargs = {
-                'norm':matplotlib.colors.LogNorm()
-            }
-
-        p = self.ax.pcolormesh(self['epoch'], self.WFR_frequencies, 
-            self[component].T, shading='auto', **pcolormesh_kwargs)
-
-        if fce:
-            mag_times, _fce = self._calc_fce()
-            for multiple in [1, 0.5, 0.1]:
-                self.ax.plot(mag_times[::100], multiple*_fce[::100], c='k')
-        return p, self.ax
-
-    def _calc_fce(self):
-        self.mag = Mag(self.sc_id, self.load_date)
-        self.mag.load()
-        # 1E-9 Convert to Tesla
-        _fce = 1E-9*self.mag.cdf['Magnitude']*np.abs(q_e)/(2*np.pi*m_e)
-        return self.mag.epoch, _fce
-
-
 class Mag:
     """
     Load a day of EMFISIS L2 magnetometer data from the computer or the internet.
@@ -195,8 +189,8 @@ class Mag:
     ----------
     sc_id: str
         The spacecraft id, can be either A or B, case insensitive.
-    load_date: datetime.datetime, pd.Timestamp
-        The date to load the data.
+    time_range: list[str, datetime.datetime, or pd.Timestamp]
+        A list, array, or tuple defining the date and time bounds to load.
 
     Methods
     -------
@@ -211,63 +205,88 @@ class Mag:
     >>> # Breneman et al., (2017) https://doi.org/10.1002/2017GL075001
     >>> q_e = -1.6E-19
     >>> m_e = 9.1E-31
-    >>> mag = Mag('A', '2016-01-20')
+    >>> mag = Mag('A', ('2016-01-20T19:00', '2016-01-20T21:00'))
     >>> mag.load()
     >>> fce = mag.fce()
     """
-    def __init__(self, sc_id, load_date) -> None:
+    def __init__(self, sc_id, time_range) -> None:
         self.sc_id = sc_id.lower()
-        if isinstance(load_date, str):
-            self.load_date = dateutil.parser.parse(load_date)
-        else:
-            self.load_date = load_date
+        self.time_range = utils.validate_time_range(time_range)
         return
 
     def load(self):
         """
         Searches for and loads the Mag data into memory.
         """
-        # rbsp-a_magnetometer_uvw_emfisis-l2_20160101_v1.6.4.cdf
-        self._file_match = (
-            f'rbsp-{self.sc_id.lower()}_magnetometer'
-            f'_uvw_emfisis-l2_{self.load_date:%Y%m%d}_v*.cdf'
-            )
-        self.file_path = self._find_file()
-        self.cdf = cdflib.CDF(self.file_path)
-        self.epoch = np.array(cdflib.cdfepoch.to_datetime(self.cdf['epoch']))
-        return self.cdf
+        file_dates = utils.get_filename_times(self.time_range, dt='days')
+        self.data = {key:np.array([]) for key in ['Magnitude', 'epoch']}
+
+        for file_date in file_dates:
+            file_path = self._find_file(file_date)
+            _cdf = cdflib.CDF(file_path)
+            self.data['epoch'] = np.concatenate((
+                self.data['epoch'],
+                np.array(cdflib.cdfepoch.to_datetime(_cdf['epoch']))
+                ))
+            self.data['Magnitude'] = np.concatenate((
+                self.data['Magnitude'], _cdf['Magnitude']
+                ))
+        
+        idt = np.where(
+            (self.data['epoch'] > self.time_range[0]) & (self.data['epoch'] <= self.time_range[1])
+            )[0]
+        self.data['epoch'] = self.data['epoch'][idt]
+        self.data['Magnitude'] = self.data['Magnitude'][idt]
+        return self.data
 
     def fce(self):
         """
         Calculate the electron gyrofrequency.
         """
-        _fce = 1E-9*self.cdf['Magnitude']*np.abs(q_e)/(2*np.pi*m_e)
+        _fce = 1E-9*self['Magnitude']*np.abs(q_e)/(2*np.pi*m_e)
         return _fce
 
-
-    def _find_file(self):
-        local_files = list(fbrbsp.config["rbsp_data_dir"].rglob(self._file_match))
+    def _find_file(self, file_date):
+        _file_match = (
+            f'rbsp-{self.sc_id.lower()}_magnetometer'
+            f'_uvw_emfisis-l2_{file_date:%Y%m%d}_v*.cdf'
+            )
+        local_files = list(fbrbsp.config["rbsp_data_dir"].rglob(_file_match))
 
         if len(local_files) == 1:
-            self.file_path = local_files[0]
+            file_path = local_files[0]
         elif len(local_files) == 0:
             # File not found locally. Check online.
             url = (base_data_url + 
                 f'rbsp{self.sc_id.lower()}/l2/emfisis/magnetometer/'
-                f'uvw/{self.load_date.year}/'
+                f'uvw/{file_date.year}/'
                 )
             downloader = sampex.Downloader(
                 url,
                 download_dir=fbrbsp.config["rbsp_data_dir"] / f'rbsp_{self.sc_id}' / 'emfisis'
                 )
-            matched_downloaders = downloader.ls(match=self._file_match)
-            self.file_path = matched_downloaders[0].download() 
+            matched_downloaders = downloader.ls(match=_file_match)
+            file_path = matched_downloaders[0].download() 
         else:
             raise FileNotFoundError(
                 f'{len(local_files)} RBSP-{self.sc_id.upper()} EMFISIS files '
-                f'found locally and online that match {self._file_match}.'
+                f'found locally and online that match {_file_match}.'
                 )
-        return self.file_path
+        return file_path
+
+    def __getitem__(self, _slice):
+        """
+        Access the cdf variables using keys (i.e., ['key']).
+        """
+        if isinstance(_slice, str):
+            if ("epoch" in _slice.lower()) or ("time" in _slice.lower()):
+                return self.data['epoch']
+            elif _slice.lower() == 'magnitude':
+                return self.data['Magnitude']
+            else:
+                raise IndexError(f'{_slice} is not in the EMFISIS Mag data.')
+        else:
+            raise IndexError(f'Only slicing with integer keys is supported.')
 
 class Burst:
     """
