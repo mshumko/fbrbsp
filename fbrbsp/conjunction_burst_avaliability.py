2530adce965b4c6d8eae7e0e225b7c7f5e93aeab
Mike Shumko <mike.shumko@jhuapl.edu>
2024-05-23 12:59:15 -0400

    Cleaned up script.

--- fbrbsp/conjunction_burst_avaliability.py
+++ fbrbsp/conjunction_burst_avaliability.py
@@ -3,6 +3,7 @@ Plot a grid of dials showing the amount of EMFISIS and EFW burst data available
 during each conjunction. 
 """
 import pathlib
+import string
 
 from datetime import datetime
 import numpy as np
@@ -12,14 +13,11 @@ import pandas as pd
 import fbrbsp
 from fbrbsp.dial import Dial
 
-ef='efw' 
-RB='RBSPa'
+RB='RBSPA'
 FU='FU3'
-df='1'
 
 file_dir = pathlib.Path(fbrbsp.__file__).parents[1] / 'data'
-#file = 'RBSPa_FU3_conjunction_values_hr.txt'   --Aaron's version. 
-file_name = 'RBSPa_FU3_conjunction_values_hr.csv'    #Preeti's version
+file_name = f'{RB}_{FU}_conjunction_values_hr.csv'    #Preeti's version
 file_path = file_dir / file_name
 assert file_path.exists()
 data = pd.read_csv(file_dir / file_name, delimiter=',',skiprows=0)
@@ -30,17 +28,17 @@ dt_closest = [datetime.strptime(data['Closest_Date_time'][i], "%Y-%m-%dT%H:%M:%S
 dt_start = [datetime.strptime(data['Start_Date_time'][i], "%Y-%m-%dT%H:%M:%S") for i in range(ntimes)]
 dt_stop = [datetime.strptime(data['Stop_Date_time'][i], "%Y-%m-%dT%H:%M:%S") for i in range(ntimes)]
 
-#Burst data and conjunctions
-plt.plot(dt_closest,data['EMFb'],'*',dt_closest,data['B1b'],'*',dt_closest,data['B2b'],'*')
-plt.yscale('log')
-plt.ylabel('seconds of burst data\nBlue=EMFISIS\nOrange=EFW B1; Green=EFW B2')
-plt.title(RB+' - '+FU + ' conjunctions')
+# #Burst data and conjunctions
+# plt.plot(dt_closest,data['EMFb'],'*',dt_closest,data['B1b'],'*',dt_closest,data['B2b'],'*')
+# plt.yscale('log')
+# plt.ylabel('seconds of burst data\nBlue=EMFISIS\nOrange=EFW B1; Green=EFW B2')
+# plt.title(RB+' - '+FU + ' conjunctions')
 
-#Chorus lowerband amplitude and conjunctions
-plt.plot(dt_closest,data['SpecBMax_lb'],'*')
-plt.yscale('log')
-plt.ylabel('Lower band chorus amp (nT^2/Hz)\nfrom +/- 1 hr of conjunction')
-plt.title(RB+' - '+FU + ' conjunctions')
+# #Chorus lowerband amplitude and conjunctions
+# plt.plot(dt_closest,data['SpecBMax_lb'],'*')
+# plt.yscale('log')
+# plt.ylabel('Lower band chorus amp (nT^2/Hz)\nfrom +/- 1 hr of conjunction')
+# plt.title(RB+' - '+FU + ' conjunctions')
 
 #-------------------------------------------------------------------
 #Make scatter plots of interesting values for Mike Shumko
@@ -53,11 +51,6 @@ b2b[b2b == 0] = np.nan
 logv = np.log(data['SpecBMax_lb'])
 logb1sec = np.log(data['B1b'])
 logb2sec = np.log(data['B2b'])
-
-ef='efw' 
-RB='RBSPa'
-FU='FU3'
-df='1'
 
 fig=plt.figure(figsize=(8,8))
 ax = np.zeros((2,2), dtype=object)
@@ -73,24 +66,31 @@ for ax_i in ax.flatten():
     ax_i.grid(False) 
     _dial.draw_earth()
     _dial._plot_params()
-# s = ax.scatter(self.MLT, self.L, c=self.minMLT, vmin=0, vmax=1)
-fig.suptitle('All conjunctions\n'+RB+'-'+FU)
-p1 = ax[0, 0].scatter(data['MLTrb'],data['Lrb'],c=data['EMFb'],vmin=0,vmax=np.nanmax(data['EMFb'])/2)
-p2 = ax[0, 1].scatter(data['MLTrb'],data['Lrb'],c=b1b,vmin=np.nanmin(b1b),vmax=np.nanmax(b1b)/1.5)
-p3 = ax[1, 0].scatter(data['MLTrb'],data['Lrb'],c=b2b,vmin=np.nanmin(b2b),vmax=np.nanmax(b2b)/2)
-p4 = ax[1, 1].scatter(data['MLTrb'],data['Lrb'],c=logv,vmin=np.min(logv),vmax=np.max(logv))
-# axs[0].set_ylabel('L')
-# axs[1].set_ylabel('L')
-# axs[2].set_ylabel('L')
-# axs[3].set_ylabel('L')
-# axs[0].set_xlabel('MLT')
-# axs[1].set_xlabel('MLT')
-# axs[2].set_xlabel('MLT')
-# axs[3].set_xlabel('MLT')
 
-# plt.colorbar(p1).ax.set_ylabel('EMFISIS burst data\n[sec]',rotation=90)
-# plt.colorbar(p2).ax.set_ylabel('EFW burst 1 data\n[sec]',rotation=90)
-# plt.colorbar(p3).ax.set_ylabel('EFW burst 2 data\n[sec]',rotation=90)
-# plt.colorbar(p4).ax.set_ylabel('chorus lower band\npeak amp\n[log10(pT^2/Hz)]')
+fig.suptitle(f'Wave Burst Availability during {RB}-{FU} conjunctions')
+scatter_objects = np.zeros_like(ax)
+scatter_objects[0, 0] = ax[0, 0].scatter(data['MLTrb'],data['Lrb'],c=data['EMFb'],vmin=0,vmax=np.nanmax(data['EMFb'])/2)
+scatter_objects[0, 1] = ax[0, 1].scatter(data['MLTrb'],data['Lrb'],c=b1b,vmin=np.nanmin(b1b),vmax=np.nanmax(b1b)/1.5)
+scatter_objects[1, 0] = ax[1, 0].scatter(data['MLTrb'],data['Lrb'],c=b2b,vmin=np.nanmin(b2b),vmax=np.nanmax(b2b)/2)
+scatter_objects[1, 1] = ax[1, 1].scatter(data['MLTrb'],data['Lrb'],c=logv,vmin=np.min(logv),vmax=np.max(logv))
+
+titles = [
+    'EMFISIS burst data',
+    'EFW burst 1 data',
+    'EFW burst 2 data',
+    'Peak Amplitude of lower-band chorus'
+]
+colorbar_labels = [
+    '[sec]',
+    '[sec]',
+    '[sec]',
+    '[log10(pT^2/Hz)]'
+]
+_z = zip(ax.flatten(), scatter_objects.flatten(), string.ascii_uppercase, titles, colorbar_labels)
+for ax_i, scatter_object, title_label, title, label in _z:
+    plt.colorbar(scatter_object, ax=ax_i, label=label)
+    ax_i.set_title(f'({title_label}) {title}')
+
+plt.tight_layout()
 plt.show()
 
